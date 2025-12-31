0ea8d34724e9620004db9dffe14d222e9498933d
goukey <44106685+goukey@users.noreply.github.com>
2025-12-31 11:52:09 +0800

    Enable Bandix traffic monitoring package

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -58,5 +58,5 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier"
 # IPSec VPN 服务器
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
 # Bandix流量监控 by timsaya
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-bandix luci-i18n-bandix-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-bandix luci-i18n-bandix-zh-cn"
 
